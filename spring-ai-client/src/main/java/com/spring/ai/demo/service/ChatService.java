eb3e0bfb14357c286a5331806a6ff659ff61ee01
udgarkhanal <khanaludgar@gmail.com>
2026-09-08 10:48:43 +0545

    add open router configuration instead of mistral ai

--- spring-ai-client/src/main/java/com/spring/ai/demo/service/ChatService.java
+++ spring-ai-client/src/main/java/com/spring/ai/demo/service/ChatService.java
@@ -1,7 +1,9 @@
 package com.spring.ai.demo.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -12,11 +14,11 @@ public class ChatService {
     private final ChatClient chatClient;
 
     public ChatService(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+        this.chatClient = builder.defaultOptions(ChatOptions.builder().model("mistral-small-2603").build()).build();
     }
 
     public Flux<String> ask() {
-        return chatClient.prompt().user("Tell me about yourself").stream().content();
+        return chatClient.prompt().user("Say hello").stream().content();
     }
 
     public Flux<String> streamChat(String prompt) {
